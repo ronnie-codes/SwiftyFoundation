6e4d60044cca56307160ebee4a9ec642daab6b42
Ronnie Vega <ronnie.vega@adieona.com>
2023-07-10 17:58:52 -0700

    Add platform versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyFoundation",
+    platforms: [
+        .macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9)
+    ],
     products: [
         .library(
             name: "SwiftyViews",
