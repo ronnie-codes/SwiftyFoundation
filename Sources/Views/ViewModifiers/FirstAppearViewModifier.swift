ae5b29c9c0ce3420bd7dc2ad6882e71d77494856
Ronny Vega <rinnyv006+github@gmail.com>
2025-11-21 20:53:59 -0600

    Add async await API for FirstAppearViewModifier

--- Sources/Views/ViewModifiers/FirstAppearViewModifier.swift
+++ Sources/Views/ViewModifiers/FirstAppearViewModifier.swift
@@ -7,24 +7,44 @@
 
 import SwiftUI
 
-private struct FirstAppearViewModifier: ViewModifier {
-    @State private var isFirstAppear: Bool = true
+private struct FirstOnAppearViewModifier: ViewModifier {
+    @State private var isFirst: Bool = true
     let action: () -> Void
 
     func body(content: Content) -> some View {
         content
             .onAppear {
-                guard isFirstAppear else {
+                guard isFirst else {
                     return
                 }
-                isFirstAppear = false
+                isFirst = false
                 action()
             }
     }
 }
 
+private struct FirstTaskViewModifier: ViewModifier {
+    @State private var isFirst: Bool = true
+    let action: () async -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .task {
+                guard isFirst else {
+                    return
+                }
+                isFirst = false
+                await action()
+            }
+    }
+}
+
 public extension View {
-    public func onFirstAppear(perform action: @escaping () -> Void) -> some View {
-        modifier(FirstAppearViewModifier(action: action))
+    func onFirstAppear(perform action: @escaping () -> Void) -> some View {
+        modifier(FirstOnAppearViewModifier(action: action))
+    }
+
+    func onFirstAppear(perform action: @escaping () async -> Void) -> some View {
+       modifier(FirstTaskViewModifier(action: action))
     }
 }
